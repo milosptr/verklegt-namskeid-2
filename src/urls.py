b8dade2930a097c26a1886b32275496b12b4fbd1
Dagur Freyr Guðbrandsson <dagur.freyr2820@gmail.com>
2024-05-06 18:10:05 +0000

    initial commit for Companies page

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views.views import home
 from .views.views import about_us
 from .views.views import application_guide
+from .views.views import companies
 # This will be our main urls file
 # We will include all the urls from the apps here
 # Look at it as the main router of the application
@@ -12,4 +13,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('about-us', about_us, name='about_us'),
     path('application-guide', application_guide, name='application_guide'),
+    path('companies', companies, name='companies')
 ]
