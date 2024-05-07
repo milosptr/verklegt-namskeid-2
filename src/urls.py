9421ced50198ac0da33b9eca062a83409290af54
Dagur Freyr Guðbrandsson <dagur.freyr2820@gmail.com>
2024-05-07 14:34:57 +0000

    more fixes

--- src/urls.py
+++ src/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 
-<<<<<<< HEAD
 from .views.views import home, contact_us
-
-=======
 from .views.views import home
 from .views.views import about_us
 from .views.views import application_guide
 from .views.views import companies
->>>>>>> f17498093ef41472b18691215f90621a6eee4486
 # This will be our main urls file
 # We will include all the urls from the apps here
 # Look at it as the main router of the application
@@ -16,12 +12,8 @@ from .views.views import companies
 
 urlpatterns = [
     path('', home, name='home'),
-<<<<<<< HEAD
-    path('contact_us', contact_us, name='contact_us')
-    ]
-=======
+    path('contact_us', contact_us, name='contact_us'),
     path('about-us', about_us, name='about_us'),
     path('application-guide', application_guide, name='application_guide'),
     path('companies', companies, name='companies')
 ]
->>>>>>> f17498093ef41472b18691215f90621a6eee4486
