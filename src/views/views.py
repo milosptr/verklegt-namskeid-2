8cee62beb042734518ae8647c3f89af70d487cc2
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2024-05-16 14:54:16 +0000

    Fixing Job offers on company_details_page

--- src/views/views.py
+++ src/views/views.py
@@ -118,8 +118,15 @@ def company_profile(request, company_name):
 
 def company_details(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    return GeneralViewController(request).render('pages/company_details.html', {'company': company})
+    
+    job_list = Job.get_by_company(company_id)
 
+    print ("Job List: ",job_list)
+    
+    return GeneralViewController(request).render('pages/company_details.html', {
+        'company': company,
+        'job_list': job_list
+    })
 
 def job_offer(request):
     return GeneralViewController(request).render('pages/job_offer.html')
