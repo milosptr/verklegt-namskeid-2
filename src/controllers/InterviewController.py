f10d2ccea22d6f91691401bcff94c31533cc2ffc
Tinna Maria <tinnamariathorleifsdottir@Tinnas-MacBook-Pro.local>
2024-05-16 20:37:42 +0000

    dashboard candidates

---
+++ src/controllers/InterviewController.py
@@ -0,0 +1,15 @@
+from src.models import interview
+
+
+class JobController:
+    def __init__(self):
+        self.job_service = Job()
+
+    def get_all(self):
+        return self.job_service.get_all()
+
+    def get_by_id(self, job_id):
+        return self.job_service.get_by_id(job_id)
+
+    def get_by_category(self, job_category_id):
+        return Job.objects.filter(category_id=job_category_id)
