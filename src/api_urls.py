11f86a736895cb655d02ac3f3cc1534349c0bda1
Tinna Maria <tinnamariathorleifsdottir@Tinnas-MacBook-Pro.local>
2024-05-17 16:19:58 +0000

    Report bug backend

--- src/api_urls.py
+++ src/api_urls.py
@@ -4,6 +4,7 @@ from src.controllers.CompanyController import CompanyController
 from src.controllers.JobController import JobController
 from src.controllers.UserController import UserController
 from src.controllers.EmailController import EmailController
+from src.controllers.BugReportController import BugReportController
 
 prefix = 'api/v1'
 
@@ -12,6 +13,9 @@ urlpatterns = [
     path(f'{prefix}/create-business-account', UserController.create_business_account_view, name='create_business_account'),
     path(f'{prefix}/login', UserController.login, name='login'),
     path(f'{prefix}/reset-password', EmailController.reset_password, name='reset_password'),
+
+    # Report bug
+    path(f'{prefix}/report-bug', BugReportController.report_bug, name='report_bug'),
 
     # Company routes
     path(f'{prefix}/company/<int:id>/update-info', CompanyController.upload_info, name='upload_info'),
